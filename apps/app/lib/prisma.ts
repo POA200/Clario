ebdc50ac4f5a6968218cd58b80abbf925f1b0828
peter oluwaseyi <peterseyi1010@gmail.com>
2026-09-01 17:17:05 +0100

    fix

--- apps/app/lib/prisma.ts
+++ apps/app/lib/prisma.ts
@@ -7,7 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
-  connectTimeoutMillis: 60000,
+  connectionTimeoutMillis: 60000,
   idleTimeoutMillis: 90000,
   query_timeout: 60000,
   max: 10,
